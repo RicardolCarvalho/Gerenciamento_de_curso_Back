0331804e6dc22fbfa3310ebde33a7284eb9a1217
Mateus Porto <mateusppp96@gmail.com>
2025-05-05 12:51:42 -0300

    fix

--- src/main/java/com/insper/cursos/config/AuthorizationConfig.java
+++ src/main/java/com/insper/cursos/config/AuthorizationConfig.java
@@ -2,6 +2,7 @@ package com.insper.cursos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,18 +19,28 @@ public class AuthorizationConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("/api/cursos/**").hasRole("ADMIN")
-                .requestMatchers("/api/avaliacoes/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .csrf(AbstractHttpConfigurer::disable)
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt
-                    .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                .authorizeHttpRequests(authorize -> authorize
+
+                        .requestMatchers("/public/**").permitAll()
+
+
+                        .requestMatchers(HttpMethod.GET, "/api/cursos/**", "/api/avaliacoes/**")
+                        .authenticated()
+
+
+                        .requestMatchers(HttpMethod.POST,   "/api/cursos/**", "/api/avaliacoes/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,    "/api/cursos/**", "/api/avaliacoes/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/cursos/**", "/api/avaliacoes/**").hasRole("ADMIN")
+
+
+                        .anyRequest().authenticated()
                 )
-            );
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                );
 
         return http.build();
     }
@@ -44,4 +55,4 @@ public class AuthorizationConfig {
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
         return jwtAuthenticationConverter;
     }
-} 
+}
